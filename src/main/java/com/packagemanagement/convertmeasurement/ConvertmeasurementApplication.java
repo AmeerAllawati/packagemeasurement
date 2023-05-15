f97f6aa19bc263226c637130125576597f185c20
AmeerAllawati <amaab@ucdavis.com>
2023-05-15 10:42:56 +0400

    Initial commit

---
+++ src/main/java/com/packagemanagement/convertmeasurement/ConvertmeasurementApplication.java
@@ -0,0 +1,13 @@
+package com.packagemanagement.convertmeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertmeasurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertmeasurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packagemanagement/convertmeasurement/ConvertmeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packagemanagement.convertmeasurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertmeasurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
